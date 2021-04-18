adfb757f62b83ca75f9654f7f5f6c7f3ac268710
Rainbow Dash <76842862+Beautiful-Emerald@users.noreply.github.com>
2021-04-18 09:45:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function Giza(){
+window.alert("The Giza Pyramid Complex, also called the Giza Necropolis, is the site on the Giza Plateau in Greater Cairo, Egypt that includes the Great Pyramid of Giza, the Pyramid of Khafre, and the Pyramid of Menkaure, along with their associated pyramid complexes and the Great Sphinx of Giza.")
+}
+
+function Rhodes(){
+window.alert("Rhodes, the largest of Greece’s Dodecanese islands, is known for its beach resorts, ancient ruins and remnants of its occupation by the Knights of St. John during the Crusades. The city of Rhodes has an Old Town featuring the medieval Street of the Knights and the castlelike Palace of the Grand Masters. Captured by the Ottomans and then held by the Italians, the palace is now a history museum.")
+}
+
+function Taj_Mahal(){
+
+window.alert("The Taj Mahal is an ivory-white marble mausoleum on the southern bank of the river Yamuna in the Indian city of Agra. It was commissioned in 1632 by the Mughal emperor Shah Jahan to house the tomb of his favourite wife, Mumtaz Mahal; it also houses the tomb of Shah Jahan himself.");
+}
+
+function Statue_Liberty(){
+
+    window.alert("The Statue of Liberty is a colossal neoclassical sculpture on Liberty Island in New York Harbor within New York City, in the United States.")
+}
+
+function China(){
+
+    window.alert("The Great Wall of China is a series of fortifications that were built across the historical northern borders of ancient Chinese states and Imperial China as protection against various nomadic groups from the Eurasian Steppe")
+}
+
+function Petra(){
+
+    window.alert("Petra is a famous archaeological site in Jordan's southwestern desert. Dating to around 300 B.C., it was the capital of the Nabatean Kingdom. Accessed via a narrow canyon called Al Siq, it contains tombs and temples carved into pink sandstone cliffs, earning its nickname, the Rose City. Perhaps its most famous structure is 45m-high Al Khazneh, a temple with an ornate, Greek-style facade, and known as The Treasury.")
+}
+
+function Colosseum(){
+
+    window.alert("The Colosseum, is an oval amphitheatre in the centre of the city of Rome, Italy, just east of the Roman Forum and is the largest ancient amphitheatre ever built, and is still the largest standing amphitheater in the world today, despite its age.")
+}
